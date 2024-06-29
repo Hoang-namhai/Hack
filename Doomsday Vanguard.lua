480fc7f25cac61ac59788c61a0b49d060526c40b
Hoang-namhai <147267934+Hoang-namhai@users.noreply.github.com>
2024-06-30 00:36:56 +0700

    Add files via upload

---
+++ Doomsday Vanguard.lua
@@ -0,0 +1,486 @@
+local info = gg.getTargetPackage() 
+if info == "com.doomsdayvanguard.survivor" then 
+else
+os.exit(print("♥𝑯𝒂̉𝒊 𝑺𝒄𝒓𝒊𝒑𝒕☠️\n \n \n• Vᴜɪ Lᴏ̀ɴɢ Cʜᴏ̣ɴ Đᴜ́ɴɢ Tʀᴏ̀ Cʜᴏ̛ɪ Cᴜ̉ᴀ Sᴄʀɪᴘᴛ •"))
+end 
+
+
+function Main()
+HNH = gg.multiChoice({
+A .. "", 
+B .. "",
+C .. "",
+D .. "",
+F .. "",
+"🔘  [EXIT]"},nil,"❤Yᴏᴜᴛᴜʙᴇ :: ʜᴀ̉ɪ sᴄʀɪᴘᴛ🍀 ᴠᴇʀsɪᴏɴ 1.0.30 X64!\
+🔥Zᴀʟᴏ: 0375574755📲 ┆Mᴇɴᴜ Hᴀᴄᴋ Doomsday Vanguard!\
+  ")
+if HNH == nil then else
+if HNH[1] == true then SpeedGame() end
+if HNH[2] == true then OneHit() end
+if HNH[3] == true then GodMod() end
+if HNH[4] == true then MoveSpeed() end
+if HNH[5] == true then NoAds() end
+if HNH[6] == true then Exit() end
+end
+XGCK = -1
+end
+
+A ="🔘 Speed Game 💕【✔】" 
+function SpeedGame() 
+if A == "🔘 Speed Game 💕【✔】" then
+gg.clearResults()
+gg.clearList()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("4417130516435348139", gg.TYPE_QWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+local t = gg.getResults(888, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.addListItems(t)
+t = nil
+local copy = false
+local t = gg.getListItems()
+if not copy then gg.removeListItems(t) end
+for i, v in ipairs(t) do
+	v.address = v.address + 0xfffffffffffffffc
+	if copy then v.name = v.name..' #2' end
+end
+gg.addListItems(t)
+t = nil
+copy = nil
+
+	gg.getListItems(t)
+	local input = gg.prompt({"SET SPEED ⏩\nSelect 6 To Reset To Original Value🔱 [1;6]"}, {1}, {'number'})
+	if input == nil then return nil
+	else
+		local t = tonumber(input[1])
+		                  gg.toast('▶️ DEFAULT ▶️')	
+		                  
+		if t == 2 then t = gg.processResume()
+revert = gg.getListItems()
+local t = gg.getListItems()
+for i, v in ipairs(t) do
+	if v.flags == gg.TYPE_QWORD then
+		v.value = "4515609228882214912"
+		v.freeze = true
+		v.freezeType = gg.FREEZE_NORMAL
+	end
+end
+gg.addListItems(t)
+t = nil
+                          gg.toast(' SPEED ⏩⏩')
+                          gg.clearResults()
+                          
+		elseif t == 3 then t = gg.processResume()
+revert = gg.getListItems()
+local t = gg.getListItems()
+for i, v in ipairs(t) do
+	if v.flags == gg.TYPE_QWORD then
+		v.value = "4515609228886409216"
+		v.freeze = true
+		v.freezeType = gg.FREEZE_NORMAL
+	end
+end
+gg.addListItems(t)
+t = nil
+		                  gg.toast(' SPEED ⏭⏩')
+		                  gg.clearResults()
+                          
+		elseif t == 4 then t = gg.processResume()
+revert = gg.getListItems()
+local t = gg.getListItems()
+for i, v in ipairs(t) do
+	if v.flags == gg.TYPE_QWORD then
+		v.value = "4515609228890603520"
+		v.freeze = true
+		v.freezeType = gg.FREEZE_NORMAL
+	end
+end
+gg.addListItems(t)
+t = nil
+		                  gg.toast(' SPEED ⏭⏩')
+		                  gg.clearResults()
+                          
+		elseif t == 5 then t = gg.processResume()
+revert = gg.getListItems()
+local t = gg.getListItems()
+for i, v in ipairs(t) do
+	if v.flags == gg.TYPE_QWORD then
+		v.value = "4515609228892700672"
+		v.freeze = true
+		v.freezeType = gg.FREEZE_NORMAL
+	end
+end
+gg.addListItems(t)
+t = nil
+gg.toast(' SPEED ⏩⏩')
+gg.clearResults()
+
+
+	elseif t == 6 then t = gg.processResume()
+revert = gg.getListItems()
+local t = gg.getListItems()
+for i, v in ipairs(t) do
+	if v.flags == gg.TYPE_QWORD then
+		v.value = "4515609228873826304"
+		v.freeze = true
+		v.freezeType = gg.FREEZE_NORMAL
+	end
+end
+gg.addListItems(t)
+t = nil                
+gg.toast('🔧Đã Reset Tốc Độ Về Ban Đầu⚖️')
+gg.clearResults()
+end
+end
+
+A = "🔘 Speed Game 💕【✔】" 
+elseif A == "🔘 Speed Game 💕【✔】" then 
+gg.clearResults()
+gg.clearList()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("4417130516435348139", gg.TYPE_QWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+local t = gg.getResults(888, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.addListItems(t)
+t = nil
+local copy = false
+local t = gg.getListItems()
+if not copy then gg.removeListItems(t) end
+for i, v in ipairs(t) do
+	v.address = v.address + 0xfffffffffffffffc
+	if copy then v.name = v.name..' #2' end
+end
+gg.addListItems(t)
+t = nil
+copy = nil
+
+	gg.getListItems(t)
+	local input = gg.prompt({"SET SPEED ⏩\nSelect 6 To Reset To Original Value🔱 [1;6]"}, {1}, {'number'})
+	if input == nil then return nil
+	else
+		local t = tonumber(input[1])
+		                  gg.toast('▶️ DEFAULT ▶️')	
+		                  
+		if t == 2 then t = gg.processResume()
+revert = gg.getListItems()
+local t = gg.getListItems()
+for i, v in ipairs(t) do
+	if v.flags == gg.TYPE_QWORD then
+		v.value = "4515609228882214912"
+		v.freeze = true
+		v.freezeType = gg.FREEZE_NORMAL
+	end
+end
+gg.addListItems(t)
+t = nil
+                          gg.toast(' SPEED ⏩⏩')
+                          gg.clearResults()
+                          
+		elseif t == 3 then t = gg.processResume()
+revert = gg.getListItems()
+local t = gg.getListItems()
+for i, v in ipairs(t) do
+	if v.flags == gg.TYPE_QWORD then
+		v.value = "4515609228886409216"
+		v.freeze = true
+		v.freezeType = gg.FREEZE_NORMAL
+	end
+end
+gg.addListItems(t)
+t = nil
+		                  gg.toast(' SPEED ⏭⏩')
+		                  gg.clearResults()
+                          
+		elseif t == 4 then t = gg.processResume()
+revert = gg.getListItems()
+local t = gg.getListItems()
+for i, v in ipairs(t) do
+	if v.flags == gg.TYPE_QWORD then
+		v.value = "4515609228890603520"
+		v.freeze = true
+		v.freezeType = gg.FREEZE_NORMAL
+	end
+end
+gg.addListItems(t)
+t = nil
+		                  gg.toast(' SPEED ⏭⏩')
+		                  gg.clearResults()
+                          
+		elseif t == 5 then t = gg.processResume()
+revert = gg.getListItems()
+local t = gg.getListItems()
+for i, v in ipairs(t) do
+	if v.flags == gg.TYPE_QWORD then
+		v.value = "4515609228892700672"
+		v.freeze = true
+		v.freezeType = gg.FREEZE_NORMAL
+	end
+end
+gg.addListItems(t)
+t = nil
+gg.toast(' SPEED ⏩⏩')
+gg.clearResults()
+
+
+	elseif t == 6 then t = gg.processResume()
+revert = gg.getListItems()
+local t = gg.getListItems()
+for i, v in ipairs(t) do
+	if v.flags == gg.TYPE_QWORD then
+		v.value = "4515609228873826304"
+		v.freeze = true
+		v.freezeType = gg.FREEZE_NORMAL
+	end
+end
+gg.addListItems(t)
+t = nil                
+gg.toast('🔧Đã Reset Tốc Độ Về Ban Đầu⚖️')
+gg.clearResults()
+end
+end
+
+A ="🔘 Speed Game 💕【✔】"
+end
+end
+
+
+
+
+
+B ="🔘 One Hit 💥【✖】" 
+function OneHit() 
+if B == "🔘 One Hit 💥【✖】" then
+ACKA01=gg.getRangesList('libil2cpp.so')[2].start
+APEX=nil  APEX={}
+APEX[1]={}
+APEX[1].address=ACKA01+0x1D9EDB4+0
+APEX[1].value='528AC700h'
+APEX[1].flags=4
+APEX[2]={}
+APEX[2].address=ACKA01+0x1D9EDB4+4
+APEX[2].value='72A0A980h'
+APEX[2].flags=4
+APEX[3]={}
+APEX[3].address=ACKA01+0x1D9EDB4+8
+APEX[3].value='D65F03C0h'
+APEX[3].flags=4
+gg.setValues(APEX)
+
+ACKA01=gg.getRangesList('libil2cpp.so')[2].start
+APEX=nil  APEX={}
+APEX[1]={}
+APEX[1].address=ACKA01+0x1D9E4B0+0
+APEX[1].value='528AC700h'
+APEX[1].flags=4
+APEX[2]={}
+APEX[2].address=ACKA01+0x1D9E4B0+4
+APEX[2].value='72A0A980h'
+APEX[2].flags=4
+APEX[3]={}
+APEX[3].address=ACKA01+0x1D9E4B0+8
+APEX[3].value='D65F03C0h'
+APEX[3].flags=4
+gg.setValues(APEX)
+gg.alert('🔥Hᴀᴄᴋ Dᴏɴᴇ🔥')
+
+B = "🔘 One Hit 💥【✔】" 
+elseif B == "🔘 One Hit 💥【✔】" then 
+ACKA01=gg.getRangesList('libil2cpp.so')[2].start
+APEX=nil  APEX={}
+APEX[1]={}
+APEX[1].address=ACKA01+0x1D9EDB4+0
+APEX[1].value='FC1E0FE8h'
+APEX[1].flags=4
+APEX[2]={}
+APEX[2].address=ACKA01+0x1D9EDB4+4
+APEX[2].value='F90007F3h'
+APEX[2].flags=4
+APEX[3]={}
+APEX[3].address=ACKA01+0x1D9EDB4+8
+APEX[3].value='A9017BFDh'
+APEX[3].flags=4
+gg.setValues(APEX)
+
+ACKA01=gg.getRangesList('libil2cpp.so')[2].start
+APEX=nil  APEX={}
+APEX[1]={}
+APEX[1].address=ACKA01+0x1D9E4B0+0
+APEX[1].value='FC1E0FE8h'
+APEX[1].flags=4
+APEX[2]={}
+APEX[2].address=ACKA01+0x1D9E4B0+4
+APEX[2].value='F90007F3h'
+APEX[2].flags=4
+APEX[3]={}
+APEX[3].address=ACKA01+0x1D9E4B0+8
+APEX[3].value='A9017BFDh'
+APEX[3].flags=4
+gg.setValues(APEX)
+gg.alert('🔥Hᴀᴄᴋ Dᴏɴᴇ🔥')
+
+B ="🔘 One Hit 💥【✖】"
+end
+end
+
+
+
+
+
+C ="🔘 God Mod ☠【✖】" 
+function GodMod() 
+if C == "🔘 God Mod ☠【✖】" then
+ACKA01=gg.getRangesList('libil2cpp.so')[2].start
+APEX=nil  APEX={}
+APEX[1]={}
+APEX[1].address=ACKA01+0x1D9EE28+0
+APEX[1].value='528AC700h'
+APEX[1].flags=4
+APEX[2]={}
+APEX[2].address=ACKA01+0x1D9EE28+4
+APEX[2].value='72A0A980h'
+APEX[2].flags=4
+APEX[3]={}
+APEX[3].address=ACKA01+0x1D9EE28+8
+APEX[3].value='D65F03C0h'
+APEX[3].flags=4
+gg.setValues(APEX)
+gg.alert('🔥Hᴀᴄᴋ Dᴏɴᴇ🔥')
+
+C = "🔘 God Mod ☠【✔】" 
+elseif C == "🔘 God Mod ☠【✔】" then 
+ACKA01=gg.getRangesList('libil2cpp.so')[2].start
+APEX=nil  APEX={}
+APEX[1]={}
+APEX[1].address=ACKA01+0x1D9EE28+0
+APEX[1].value='FC1E0FE8h'
+APEX[1].flags=4
+APEX[2]={}
+APEX[2].address=ACKA01+0x1D9EE28+4
+APEX[2].value='F90007F3h'
+APEX[2].flags=4
+APEX[3]={}
+APEX[3].address=ACKA01+0x1D9EE28+8
+APEX[3].value='A9017BFDh'
+APEX[3].flags=4
+gg.setValues(APEX)
+gg.alert('🔥Hᴀᴄᴋ Dᴏɴᴇ🔥')
+
+C ="🔘 God Mod ☠【✖】"
+end
+end
+
+
+
+
+
+D ="🔘 Move Speed 💨【✖】" 
+function MoveSpeed() 
+if D == "🔘 Move Speed 💨【✖】" then
+ACKA01=gg.getRangesList('libil2cpp.so')[2].start
+APEX=nil  APEX={}
+APEX[1]={}
+APEX[1].address=ACKA01+0x1D9E1A8+0
+APEX[1].value='52800000h'
+APEX[1].flags=4
+APEX[2]={}
+APEX[2].address=ACKA01+0x1D9E1A8+4
+APEX[2].value='72A82E00h'
+APEX[2].flags=4
+APEX[3]={}
+APEX[3].address=ACKA01+0x1D9E1A8+8
+APEX[3].value='1E270000h'
+APEX[3].flags=4
+APEX[4]={}
+APEX[4].address=ACKA01+0x1D9E1A8+12
+APEX[4].value='D65F03C0h'
+APEX[4].flags=4
+gg.setValues(APEX)
+gg.alert('🔥Hᴀᴄᴋ Dᴏɴᴇ🔥')
+
+D = "🔘 Move Speed 💨【✔】" 
+elseif D == "🔘 Move Speed 💨【✔】" then 
+ACKA01=gg.getRangesList('libil2cpp.so')[2].start
+APEX=nil  APEX={}
+APEX[1]={}
+APEX[1].address=ACKA01+0x1D9E1A8+0
+APEX[1].value='6DBA3BEFh'
+APEX[1].flags=4
+APEX[2]={}
+APEX[2].address=ACKA01+0x1D9E1A8+4
+APEX[2].value='6D0133EDh'
+APEX[2].flags=4
+APEX[3]={}
+APEX[3].address=ACKA01+0x1D9E1A8+8
+APEX[3].value='6D022BEBh'
+APEX[3].flags=4
+APEX[4]={}
+APEX[4].address=ACKA01+0x1D9E1A8+12
+APEX[4].value='6D0323E9h'
+APEX[4].flags=4
+gg.setValues(APEX)
+gg.alert('🔥Hᴀᴄᴋ Dᴏɴᴇ🔥')
+
+
+D ="🔘 Move Speed 💨【✖】"
+end
+end
+
+
+
+
+F ="🔘 NoAds 🚫【✖】" 
+function NoAds() 
+if F == "🔘 NoAds 🚫【✖】" then
+ACKA01=gg.getRangesList('libil2cpp.so')[2].start
+APEX=nil  APEX={}
+APEX[1]={}
+APEX[1].address=ACKA01+0x3C6204C+0
+APEX[1].value='D2800020h'
+APEX[1].flags=4
+APEX[2]={}
+APEX[2].address=ACKA01+0x3C6204C+4
+APEX[2].value='D65F03C0h'
+APEX[2].flags=4
+gg.setValues(APEX)
+gg.alert('🔥Hᴀᴄᴋ Dᴏɴᴇ🔥')
+
+F = "🔘 NoAds 🚫【✔】" 
+elseif F == "🔘 NoAds 🚫【✔】" then 
+ACKA01=gg.getRangesList('libil2cpp.so')[2].start
+APEX=nil  APEX={}
+APEX[1]={}
+APEX[1].address=ACKA01+0x3C6204C+0
+APEX[1].value='A9BF7BFDh'
+APEX[1].flags=4
+APEX[2]={}
+APEX[2].address=ACKA01+0x3C6204C+4
+APEX[2].value='910003FDh'
+APEX[2].flags=4
+gg.setValues(APEX)
+gg.alert('🔥Hᴀᴄᴋ Dᴏɴᴇ🔥')
+
+
+F ="🔘 NoAds 🚫【✖】"
+end
+end
+
+
+
+
+
+function Exit()
+gg.clearResults()
+gg.clearList()
+gg.alert('♥ᴍᴏᴅ ʙʏ ʜᴀ̉ɪ sᴄʀɪᴘᴛ♥')
+gg.alert("❤️Yᴏᴜᴛᴜʙᴇ :: ʜᴀ̉ɪ sᴄʀɪᴘᴛ🍀")
+print('☞♥Zᴀʟᴏ: 0375574755♥☜')
+os.exit()
+end
+while true do
+if gg.isVisible(true) then
+XGCK = 1
+gg.setVisible(false)
+end
+if XGCK == 1 then Main() end
+end
+
+
+
