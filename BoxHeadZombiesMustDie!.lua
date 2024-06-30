5b01c1361b00718e1675fb84be9f506a1e3de74b
Hoang-namhai <147267934+Hoang-namhai@users.noreply.github.com>
2024-06-30 18:12:39 +0700

    Add files via upload

---
+++ BoxHeadZombiesMustDie!.lua
@@ -0,0 +1,172 @@
+local info = gg.getTargetPackage() 
+if info == "com.migoigames.boxheadzombiesmustdie" then 
+else
+os.exit(print("♥𝑯𝒂̉𝒊 𝑺𝒄𝒓𝒊𝒑𝒕☠️\n \n \n• Vᴜɪ Lᴏ̀ɴɢ Cʜᴏ̣ɴ Đᴜ́ɴɢ Tʀᴏ̀ Cʜᴏ̛ɪ Cᴜ̉ᴀ Sᴄʀɪᴘᴛ •"))
+end 
+
+
+function Main()
+HNH = gg.multiChoice({
+A .. "", 
+B .. "",
+C .. "",
+"🔘  [EXIT]"},nil,"❤Yᴏᴜᴛᴜʙᴇ :: ʜᴀ̉ɪ sᴄʀɪᴘᴛ🍀 ᴠᴇʀsɪᴏɴ 2.7.8 X64!\
+🔥Zᴀʟᴏ: 0375574755📲 ┆Mᴇɴᴜ Hᴀᴄᴋ Box Head: Zombies Must Die!\
+  ")
+if HNH == nil then else
+if HNH[1] == true then UnlockAll() end
+if HNH[2] == true then OneHit() end
+if HNH[3] == true then GodMod() end
+if HNH[4] == true then Exit() end
+end
+XGCK = -1
+end
+
+A ="🔘 UnLock All 🔓【✖】" 
+function UnlockAll() 
+if A == "🔘 UnLock All 🔓【✖】" then
+ACKA01=gg.getRangesList('libil2cpp.so')[2].start
+APEX=nil  APEX={}
+APEX[1]={}
+APEX[1].address=ACKA01+0xEA23B0+0
+APEX[1].value='D2800020h'
+APEX[1].flags=4
+APEX[2]={}
+APEX[2].address=ACKA01+0xEA23B0+4
+APEX[2].value='D65F03C0h'
+APEX[2].flags=4
+gg.setValues(APEX)
+gg.alert('🔥Hᴀᴄᴋ Dᴏɴᴇ🔥')
+
+A = "🔘 UnLock All 🔓【✔】" 
+elseif A == "🔘 UnLock All 🔓【✔】" then 
+ACKA01=gg.getRangesList('libil2cpp.so')[2].start
+APEX=nil  APEX={}
+APEX[1]={}
+APEX[1].address=ACKA01+0xEA23B0+0
+APEX[1].value='A9BF7BF3h'
+APEX[1].flags=4
+APEX[2]={}
+APEX[2].address=ACKA01+0xEA23B0+4
+APEX[2].value='B9405008h'
+APEX[2].flags=4
+gg.setValues(APEX)
+gg.alert('🔥Hᴀᴄᴋ Dᴏɴᴇ🔥')
+
+A ="🔘 UnLock All 🔓【✖】"
+end
+end
+
+
+
+
+
+B ="🔘 One Hit 💥【✖】" 
+function OneHit() 
+if B == "🔘 One Hit 💥【✖】" then
+ACKA01=gg.getRangesList('libil2cpp.so')[2].start
+APEX=nil  APEX={}
+APEX[1]={}
+APEX[1].address=ACKA01+0xD5BBA4+0
+APEX[1].value='128250C0h'
+APEX[1].flags=4
+APEX[2]={}
+APEX[2].address=ACKA01+0xD5BBA4+4
+APEX[2].value='72A9CA60h'
+APEX[2].flags=4
+APEX[3]={}
+APEX[3].address=ACKA01+0xD5BBA4+8
+APEX[3].value='1E270000h'
+APEX[3].flags=4
+APEX[4]={}
+APEX[4].address=ACKA01+0xD5BBA4+12
+APEX[4].value='D65F03C0h'
+APEX[4].flags=4
+gg.setValues(APEX)
+gg.alert('🔥Hᴀᴄᴋ Dᴏɴᴇ🔥')
+
+B = "🔘 One Hit 💥【✔】" 
+elseif B == "🔘 One Hit 💥【✔】" then 
+ACKA01=gg.getRangesList('libil2cpp.so')[2].start
+APEX=nil  APEX={}
+APEX[1]={}
+APEX[1].address=ACKA01+0xD5BBA4+0
+APEX[1].value='6DBC23E9h'
+APEX[1].flags=4
+APEX[2]={}
+APEX[2].address=ACKA01+0xD5BBA4+4
+APEX[2].value='F9000BF6h'
+APEX[2].flags=4
+APEX[3]={}
+APEX[3].address=ACKA01+0xD5BBA4+8
+APEX[3].value='A90253F5h'
+APEX[3].flags=4
+APEX[4]={}
+APEX[4].address=ACKA01+0xD5BBA4+12
+APEX[4].value='A9037BF3h'
+APEX[4].flags=4
+gg.setValues(APEX)
+gg.alert('🔥Hᴀᴄᴋ Dᴏɴᴇ🔥')
+
+B ="🔘 One Hit 💥【✖】"
+end
+end
+
+
+
+
+
+C ="🔘 God Mod 💕【✖】" 
+function GodMod() 
+if C == "🔘 God Mod 💕【✖】" then
+ACKA01=gg.getRangesList('libil2cpp.so')[2].start
+APEX=nil  APEX={}
+APEX[1]={}
+APEX[1].address=ACKA01+0xDAE824+0
+APEX[1].value='D2800020h'
+APEX[1].flags=4
+APEX[2]={}
+APEX[2].address=ACKA01+0xDAE824+4
+APEX[2].value='D65F03C0h'
+APEX[2].flags=4
+gg.setValues(APEX)
+gg.alert('🔥Hᴀᴄᴋ Dᴏɴᴇ🔥')
+
+C = "🔘 God Mod 💕【✔】" 
+elseif C == "🔘 God Mod 💕【✔】" then 
+ACKA01=gg.getRangesList('libil2cpp.so')[2].start
+APEX=nil  APEX={}
+APEX[1]={}
+APEX[1].address=ACKA01+0xDAE824+0
+APEX[1].value='1E2703E0h'
+APEX[1].flags=4
+APEX[2]={}
+APEX[2].address=ACKA01+0xDAE824+4
+APEX[2].value='D65F03C0h'
+APEX[2].flags=4
+gg.setValues(APEX)
+gg.alert('🔥Hᴀᴄᴋ Dᴏɴᴇ🔥')
+
+C ="🔘 God Mod 💕【✖】"end
+end
+
+
+
+
+function Exit()
+gg.clearResults()
+gg.clearList()
+gg.alert('♥ᴍᴏᴅ ʙʏ ʜᴀ̉ɪ sᴄʀɪᴘᴛ♥')
+gg.alert("❤️Yᴏᴜᴛᴜʙᴇ :: ʜᴀ̉ɪ sᴄʀɪᴘᴛ🍀")
+print('☞♥Zᴀʟᴏ: 0375574755♥☜')
+os.exit()
+end
+while true do
+if gg.isVisible(true) then
+XGCK = 1
+gg.setVisible(false)
+end
+if XGCK == 1 then Main() end
+end
+
+
