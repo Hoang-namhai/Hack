b28f4de20209cb360dd3f3d1d498d728f2dde3ea
Hoang-namhai <147267934+Hoang-namhai@users.noreply.github.com>
2024-07-09 10:27:50 +0700

    Add files via upload

---
+++ WarriorCraftAutoChess.lua
@@ -0,0 +1,291 @@
+local info = gg.getTargetPackage() 
+ if info == "com.asgen.warcraftautochess" then 
+ else
+ os.exit(print("♥𝑯𝒂̉𝒊 𝑺𝒄𝒓𝒊𝒑𝒕☠️\n \n \n• Vᴜɪ Lᴏ̀ɴɢ Cʜᴏ̣ɴ Đᴜ́ɴɢ Tʀᴏ̀ Cʜᴏ̛ɪ Cᴜ̉ᴀ Sᴄʀɪᴘᴛ •"))
+ end 
+
+
+
+ProgressBar =  "║░░░░░░░░░░░░░░░║";for x = 1,16,1 do gg.sleep(50) ProgressBar = ProgressBar:gsub("░","▓",1);gg.toast(ProgressBar) end gg.sleep(50) 
+gg.sleep(100) 
+
+
+
+
+gg.getTargetInfo()
+gg.alert("𝙶𝙰𝙼𝙴 𝚅𝙴𝚁𝚂𝙸𝙾𝙽 : 1.3.6                                                                                                                            𝙶𝙰𝙼𝙴: 𝚆𝚊𝚛𝚛𝚒𝚘𝚛 𝙲𝚛𝚊𝚏𝚝: 𝙰𝚞𝚝𝚘 𝙲𝚑𝚎𝚜𝚜 X64                                                                                                                 𝚂𝙲𝚁𝙸𝙿𝚃 𝚅𝙴𝚁𝚂𝙸𝙾𝙽 : 1.0                                                                                                                                 𝙻𝙰𝚂𝚃 𝚄𝙿𝙳𝙰𝚃𝙴 : Mᴏɴᴅᴀʏ. Jᴜʟʏ 9. 2024                                                                                                  Mᴀᴅᴇ Bʏ Hᴏᴀ̀ɴɢ Nᴀᴍ Hᴀ̉ɪ                                                                                                                                Sᴄʀɪᴘᴛ Pᴜʙʟɪsʜᴇᴅ ɪɴ ©2024", "[𝙽𝙴𝚇𝚃]")
+
+
+ProgressBar =  "║░░░░░░░░░░░░░░░║";for x = 1,16,1 do gg.sleep(50) ProgressBar = ProgressBar:gsub("░","▓",1);gg.toast(ProgressBar) end gg.sleep(50) 
+gg.sleep(50)
+
+gg.sleep(50)
+
+
+
+
+
+
+function Main()
+HNH = gg.multiChoice({
+A .. "", 
+B .. "",
+C .. "",
+D .. "",
+F .. "",
+"[EXIT]"},nil,"❤𝗬𝗼𝘂𝘁𝘂𝗯𝗲 :: ʜᴀ̉ɪ sᴄʀɪᴘᴛ🍀\n🎮𝗚𝗮𝗺𝗲: Warrior Craft: Auto Chess X64\n💭𝗩𝗲𝗿𝘀𝗶𝗼𝗻: 1.3.6\n--------------------------------------------------------------")
+if HNH == nil then else
+if HNH[1] == true then OneHit() end
+if HNH[2] == true then SpeedX2() end
+if HNH[3] == true then MaxSlot() end
+if HNH[4] == true then UpgradeHero() end
+if HNH[5] == true then MaxLevel() end
+if HNH[6] == true then Exit() end
+end
+XGCK = -1
+end
+
+
+
+
+
+A ="[OFF]  One Hit 💥" 
+function OneHit() 
+if A == "[OFF]  One Hit 💥" then
+ACKA01=gg.getRangesList('libil2cpp.so')[2].start
+APEX=nil  APEX={}
+APEX[1]={}
+APEX[1].address=ACKA01+0x17B34CC+0
+APEX[1].value='528BC700h'
+APEX[1].flags=4
+APEX[2]={}
+APEX[2].address=ACKA01+0x17B34CC+4
+APEX[2].value='72A69F60h'
+APEX[2].flags=4
+APEX[3]={}
+APEX[3].address=ACKA01+0x17B34CC+8
+APEX[3].value='D65F03C0h'
+APEX[3].flags=4
+gg.setValues(APEX)
+gg.toast('✅One Hit✅')
+
+A = "[ON]  One Hit 💥" 
+elseif A == "[ON]  One Hit 💥" then 
+ACKA01=gg.getRangesList('libil2cpp.so')[2].start
+APEX=nil  APEX={}
+APEX[1]={}
+APEX[1].address=ACKA01+0x17B34CC+0
+APEX[1].value='D100C3FFh'
+APEX[1].flags=4
+APEX[2]={}
+APEX[2].address=ACKA01+0x17B34CC+4
+APEX[2].value='A90153F5h'
+APEX[2].flags=4
+APEX[3]={}
+APEX[3].address=ACKA01+0x17B34CC+8
+APEX[3].value='A9027BF3h'
+APEX[3].flags=4
+gg.setValues(APEX)
+gg.toast('❌One Hit❌')
+
+A ="[OFF]  One Hit 💥"
+end
+end
+
+
+
+
+
+B ="[OFF]  Speed X2 💫" 
+function SpeedX2() 
+if B == "[OFF]  Speed X2 💫" then
+ACKA01=gg.getRangesList('libil2cpp.so')[2].start
+APEX=nil  APEX={}
+APEX[1]={}
+APEX[1].address=ACKA01+0x1915514+0
+APEX[1].value='D2800020h'
+APEX[1].flags=4
+APEX[2]={}
+APEX[2].address=ACKA01+0x1915514+4
+APEX[2].value='D65F03C0h'
+APEX[2].flags=4
+gg.setValues(APEX)
+gg.toast('✅Speed X2✅')
+
+B = "[ON]  Speed X2 💫" 
+elseif B == "[ON]  Speed X2 💫" then 
+ACKA01=gg.getRangesList('libil2cpp.so')[2].start
+APEX=nil  APEX={}
+APEX[1]={}
+APEX[1].address=ACKA01+0x1915514+0
+APEX[1].value='F81E0FF4h'
+APEX[1].flags=4
+APEX[2]={}
+APEX[2].address=ACKA01+0x1915514+4
+APEX[2].value='A9017BF3h'
+APEX[2].flags=4
+gg.setValues(APEX)
+gg.toast('❌Speed X2❌')
+
+B ="[OFF]  Speed X2 💫"
+end
+end
+
+
+
+
+
+C ="[OFF]  Max 🍗🍗🍗" 
+function MaxSlot() 
+if C == "[OFF]  Max 🍗🍗🍗" then
+ACKA01=gg.getRangesList('libil2cpp.so')[2].start
+APEX=nil  APEX={}
+APEX[1]={}
+APEX[1].address=ACKA01+0x1915504+0
+APEX[1].value='528BC700h'
+APEX[1].flags=4
+APEX[2]={}
+APEX[2].address=ACKA01+0x1915504+4
+APEX[2].value='72A69F60h'
+APEX[2].flags=4
+APEX[3]={}
+APEX[3].address=ACKA01+0x1915504+8
+APEX[3].value='D65F03C0h'
+APEX[3].flags=4
+gg.setValues(APEX)
+gg.toast('✅Max 🍗🍗🍗✅')
+
+C = "[ON]  Max 🍗🍗🍗" 
+elseif C == "[ON]  Max 🍗🍗🍗" then 
+ACKA01=gg.getRangesList('libil2cpp.so')[2].start
+APEX=nil  APEX={}
+APEX[1]={}
+APEX[1].address=ACKA01+0x1915504+0
+APEX[1].value='B9405408h'
+APEX[1].flags=4
+APEX[2]={}
+APEX[2].address=ACKA01+0x1915504+4
+APEX[2].value='B9402809h'
+APEX[2].flags=4
+APEX[3]={}
+APEX[3].address=ACKA01+0x1915504+8
+APEX[3].value='0B080120h'
+APEX[3].flags=4
+gg.setValues(APEX)
+gg.toast('❌Max 🍗🍗🍗❌')
+
+C ="[OFF]  Max 🍗🍗🍗"end
+end
+
+
+
+
+
+D ="[OFF]  Upgrade Hero 💢" 
+function UpgradeHero() 
+if D == "[OFF]  Upgrade Hero 💢" then
+ACKA01=gg.getRangesList('libil2cpp.so')[2].start
+APEX=nil  APEX={}
+APEX[1]={}
+APEX[1].address=ACKA01+0x1915ADC+0
+APEX[1].value='528BC700h'
+APEX[1].flags=4
+APEX[2]={}
+APEX[2].address=ACKA01+0x1915ADC+4
+APEX[2].value='72A69F60h'
+APEX[2].flags=4
+APEX[3]={}
+APEX[3].address=ACKA01+0x1915ADC+8
+APEX[3].value='D65F03C0h'
+APEX[3].flags=4
+gg.setValues(APEX)
+gg.toast('✅Upgrade Hero✅')
+
+D = "[ON]  Upgrade Hero 💢" 
+elseif D == "[ON]  Upgrade Hero 💢" then 
+ACKA01=gg.getRangesList('libil2cpp.so')[2].start
+APEX=nil  APEX={}
+APEX[1]={}
+APEX[1].address=ACKA01+0x1915ADC+0
+APEX[1].value='D100C3FFh'
+APEX[1].flags=4
+APEX[2]={}
+APEX[2].address=ACKA01+0x1915ADC+4
+APEX[2].value='A90153F5h'
+APEX[2].flags=4
+APEX[3]={}
+APEX[3].address=ACKA01+0x1915ADC+8
+APEX[3].value='A9027BF3h'
+APEX[3].flags=4
+gg.setValues(APEX)
+gg.toast('❌Upgrade Hero❌')
+
+D ="[OFF]  Upgrade Hero 💢"end
+end
+
+
+
+
+F ="[OFF]  Max Level 🕶️" 
+function MaxLevel() 
+if F == "[OFF]  Max Level 🕶️" then
+ACKA01=gg.getRangesList('libil2cpp.so')[2].start
+APEX=nil  APEX={}
+APEX[1]={}
+APEX[1].address=ACKA01+0x17B2454+0
+APEX[1].value='528AC700h'
+APEX[1].flags=4
+APEX[2]={}
+APEX[2].address=ACKA01+0x17B2454+4
+APEX[2].value='72A0A980h'
+APEX[2].flags=4
+APEX[3]={}
+APEX[3].address=ACKA01+0x17B2454+8
+APEX[3].value='D65F03C0h'
+APEX[3].flags=4
+gg.setValues(APEX)
+gg.toast('✅Max Level✅')
+
+F = "[ON]  Max Level 🕶️" 
+elseif F == "[ON]  Max Level 🕶️" then 
+ACKA01=gg.getRangesList('libil2cpp.so')[2].start
+APEX=nil  APEX={}
+APEX[1]={}
+APEX[1].address=ACKA01+0x17B2454+0
+APEX[1].value='F81F0FFEh'
+APEX[1].flags=4
+APEX[2]={}
+APEX[2].address=ACKA01+0x17B2454+4
+APEX[2].value='F9408008h'
+APEX[2].flags=4
+APEX[3]={}
+APEX[3].address=ACKA01+0x17B2454+8
+APEX[3].value='B4000088h'
+APEX[3].flags=4
+gg.setValues(APEX)
+gg.toast('❌Max Level❌')
+
+F ="[OFF]  Max Level 🕶️"end
+end
+
+
+
+
+
+
+function Exit()
+gg.clearResults()
+gg.clearList()
+gg.toast('♥ᴍᴏᴅ ʙʏ ʜᴀ̉ɪ sᴄʀɪᴘᴛ♥')
+gg.alert("❤️Yᴏᴜᴛᴜʙᴇ :: ʜᴀ̉ɪ sᴄʀɪᴘᴛ🍀")
+print('☞♥Zᴀʟᴏ: 0375574755♥☜')
+os.exit()
+end
+while true do
+if gg.isVisible(true) then
+XGCK = 1
+gg.setVisible(false)
+end
+if XGCK == 1 then Main() end
+end
+
