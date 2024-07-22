a7fc677e95a933d2e695cb10b799082c01d70839
Hoang-namhai <147267934+Hoang-namhai@users.noreply.github.com>
2024-07-22 08:32:13 +0700

    Add files via upload

---
+++ KingDomWarTD.lua
@@ -0,0 +1,208 @@
+local info = gg.getTargetPackage() 
+ if info == "perfectplan.warring.kingdom.btd.epic.tower.defense" then 
+ else
+ os.exit(print("♥𝑯𝒂̉𝒊 𝑺𝒄𝒓𝒊𝒑𝒕☠️\n \n \n• Vᴜɪ Lᴏ̀ɴɢ Cʜᴏ̣ɴ Đᴜ́ɴɢ Tʀᴏ̀ Cʜᴏ̛ɪ Cᴜ̉ᴀ Sᴄʀɪᴘᴛ •"))
+ end 
+
+
+
+ProgressBar =  "║░░░░░░░░░░░░░░░║";for x = 1,16,1 do gg.sleep(50) ProgressBar = ProgressBar:gsub("░","▓",1);gg.toast(ProgressBar) end gg.sleep(50) 
+gg.sleep(100) 
+
+
+
+
+gg.getTargetInfo()
+gg.alert("𝙶𝙰𝙼𝙴 𝚅𝙴𝚁𝚂𝙸𝙾𝙽 : 2.1.78                                                                                                                            𝙶𝙰𝙼𝙴 𝙽𝙰𝙼𝙴 : 𝙺𝚒𝚗𝚐𝚍𝚘𝚖 𝚆𝚊𝚛: 𝚃𝚑𝚞̉ 𝚃𝚑𝚊̀𝚗𝚑 𝚃𝙳 X64                                                                                                                 𝚂𝙲𝚁𝙸𝙿𝚃 𝚅𝙴𝚁𝚂𝙸𝙾𝙽 : 1.0                                                                                                                                 𝙻𝙰𝚂𝚃 𝚄𝙿𝙳𝙰𝚃𝙴 : 𝚂𝚊𝚝𝚞𝚛𝚍𝚊𝚢, 𝙹𝚞𝚕𝚢 𝟸0, 𝟸0𝟸𝟺                                                                                                  Mᴀᴅᴇ Bʏ Hᴏᴀ̀ɴɢ Nᴀᴍ Hᴀ̉ɪ                                                                                                                                Sᴄʀɪᴘᴛ Pᴜʙʟɪsʜᴇᴅ ɪɴ ©2024", "[𝙽𝙴𝚇𝚃]")
+
+gg.alert("- 𝙻𝚞̛𝚞 𝚢́: 𝙱𝚊̣̂𝚝 𝙷𝚊𝚌𝚔 𝚇𝚘𝚗𝚐 𝙼𝚘̣𝚒 𝙽𝚐𝚞̛𝚘̛̀𝚒 𝚅𝚊̀𝚘 𝚂𝚑𝚘𝚙 𝙼𝚞𝚊 𝙼𝚘̣̂𝚝 𝙶𝚘́𝚒 𝚃𝚑𝚎̂̉ 𝙻𝚞̛̣𝚌 𝙱𝚊̂́𝚝 𝙺𝚢̀ Đ𝚎̂̉ 𝙷𝚊𝚌𝚔 Đ𝚞̛𝚘̛̣𝚌 𝙺𝚒́𝚌𝚑 𝙷𝚘𝚊̣𝚝 𝚃𝚑𝚊̀𝚗𝚑 𝙲𝚘̂𝚗𝚐 𝙽𝚑𝚎́.\n\n- 𝙽𝚘𝚝𝚎: 𝙰𝚏𝚝𝚎𝚛 𝚊𝚌𝚝𝚒𝚟𝚊𝚝𝚒𝚗𝚐 𝚝𝚑𝚎 𝙷𝚊𝚌𝚔, 𝚎𝚟𝚎𝚛𝚢𝚘𝚗𝚎 𝚜𝚑𝚘𝚞𝚕𝚍 𝚐𝚘 𝚝𝚘 𝚝𝚑𝚎 𝚂𝚑𝚘𝚙 𝚊𝚗𝚍 𝚋𝚞𝚢 𝚊𝚗𝚢 𝚂𝚝𝚊𝚖𝚒𝚗𝚊 𝙿𝚊𝚌𝚔 𝚏𝚘𝚛 𝚝𝚑𝚎 𝙷𝚊𝚌𝚔 𝚝𝚘 𝚋𝚎 𝚊𝚌𝚝𝚒𝚟𝚊𝚝𝚎𝚍 𝚜𝚞𝚌𝚌𝚎𝚜𝚜𝚏𝚞𝚕𝚕𝚢.")
+ProgressBar =  "║░░░░░░░░░░░░░░░║";for x = 1,16,1 do gg.sleep(50) ProgressBar = ProgressBar:gsub("░","▓",1);gg.toast(ProgressBar) end gg.sleep(50) 
+gg.sleep(50)
+
+gg.sleep(50)
+
+
+
+
+
+
+function Main()
+HNH = gg.multiChoice({
+A .. "",
+"[𝙴𝚇𝙸𝚃]"},nil,"❤𝗬𝗼𝘂𝘁𝘂𝗯𝗲 :: ʜᴀ̉ɪ sᴄʀɪᴘᴛ🍀\n🎮𝗚𝗮𝗺𝗲: Kingdom War: Thủ Thành TD X64\n💭𝗩𝗲𝗿𝘀𝗶𝗼𝗻: 2.1.78\n--------------------------------------------------------------")
+if HNH == nil then else
+if HNH[1] == true then FreeShopAll() end
+if HNH[2] == true then Exit() end
+end
+XGCK = -1
+end
+
+
+
+
+A ="- 𝚄𝚗𝚕𝚘𝚌𝚔 𝙲𝚑𝚎𝚊𝚝 𝙵𝚞𝚗𝚌𝚝𝚒𝚘𝚗🕶️\n- 𝙵𝚛𝚎𝚎 𝚂𝚑𝚘𝚙 𝙰𝚕𝚕🛒" 
+function FreeShopAll() 
+if A == "- 𝚄𝚗𝚕𝚘𝚌𝚔 𝙲𝚑𝚎𝚊𝚝 𝙵𝚞𝚗𝚌𝚝𝚒𝚘𝚗🕶️\n- 𝙵𝚛𝚎𝚎 𝚂𝚑𝚘𝚙 𝙰𝚕𝚕🛒" then
+function searchValue(t,hai1,hai2)
+rt={}
+gg.setRanges(hai1)
+gg.clearResults()
+gg.clearList()
+gg.setVisible(false)
+gg.searchNumber(t[1], hai2)
+local r = gg.getResults(99999999)
+if #r==0 then goto HoangNamHai end
+for it=2,#t do
+for i=1,#r do
+r[i].address=r[i].address+t[it][2]
+end
+local rr=gg.getValues(r)
+tt={}
+for i=1,#rr do
+   if rr[i].value== t[it][1] then
+   ii=#tt+1
+   tt[ii]={}
+   tt[ii].address=rr[i].address-t[it][2]
+   tt[ii].flags=4
+   end
+end
+if #tt==0 then goto HoangNamHai end
+r=gg.getValues(tt)
+if it==#t then rt=r goto HoangNamHai end
+end
+::HoangNamHai::
+return rt
+end
+
+function searchEdit(hnh1,hnh2,hnh3)
+if #r>0 then
+tt={}
+for i=1,#r do
+ii=#tt+1 tt[ii]={}
+tt[ii].address=r[i].address +hnh1
+tt[ii].flags=hnh2
+tt[ii].value=hnh3
+end
+gg.setValues(tt)
+end end
+
+r=searchValue({"1125515264",{"1120403456",48*4},{"1112014848",96*4}, {"1101004800",144*4}},gg.REGION_ANONYMOUS,gg.TYPE_QWORD)
+if #r == 0 then else
+searchEdit(48*4,32, 1065353216)
+searchEdit(96*4,32, 1065353216)
+searchEdit(144*4,32, 1065353216)
+searchEdit(23*4,32, 4294967325)
+searchEdit(24*4,32, 4294967297)
+searchEdit(25*4,32, 1)
+searchEdit(71*4,32, 4294967325)
+searchEdit(72*4,32, 4294967297)
+searchEdit(73*4,32, 1)
+searchEdit(119*4,32, 4294967325)
+searchEdit(120*4,32, 4294967297)
+searchEdit(121*4,32, 1)
+searchEdit(-23*4,32, 1)
+searchEdit(-24*4,32, 4294967297)
+searchEdit(-25*4,32, 4294967325)
+gg.clearResults()
+end
+gg.alert('✅𝙵𝚛𝚎𝚎 𝚂𝚑𝚘𝚙 𝙰𝚕𝚕 𝙰𝚌𝚝𝚒𝚟𝚊𝚝𝚎𝚍✅')
+gg.clearResults()
+
+
+
+
+A = " - 𝚄𝚗𝚕𝚘𝚌𝚔 𝙲𝚑𝚎𝚊𝚝 𝙵𝚞𝚗𝚌𝚝𝚒𝚘𝚗🕶️\n- 𝙵𝚛𝚎𝚎 𝚂𝚑𝚘𝚙 𝙰𝚕𝚕🛒" 
+elseif A == " - 𝚄𝚗𝚕𝚘𝚌𝚔 𝙲𝚑𝚎𝚊𝚝 𝙵𝚞𝚗𝚌𝚝𝚒𝚘𝚗🕶️\n- 𝙵𝚛𝚎𝚎 𝚂𝚑𝚘𝚙 𝙰𝚕𝚕🛒" then 
+function searchValue(t,hai1,hai2)
+rt={}
+gg.setRanges(hai1)
+gg.clearResults()
+gg.clearList()
+gg.setVisible(false)
+gg.searchNumber(t[1], hai2)
+local r = gg.getResults(99999999)
+if #r==0 then goto HoangNamHai end
+for it=2,#t do
+for i=1,#r do
+r[i].address=r[i].address+t[it][2]
+end
+local rr=gg.getValues(r)
+tt={}
+for i=1,#rr do
+   if rr[i].value== t[it][1] then
+   ii=#tt+1
+   tt[ii]={}
+   tt[ii].address=rr[i].address-t[it][2]
+   tt[ii].flags=4
+   end
+end
+if #tt==0 then goto HoangNamHai end
+r=gg.getValues(tt)
+if it==#t then rt=r goto HoangNamHai end
+end
+::HoangNamHai::
+return rt
+end
+
+function searchEdit(hnh1,hnh2,hnh3)
+if #r>0 then
+tt={}
+for i=1,#r do
+ii=#tt+1 tt[ii]={}
+tt[ii].address=r[i].address +hnh1
+tt[ii].flags=hnh2
+tt[ii].value=hnh3
+end
+gg.setValues(tt)
+end end
+
+r=searchValue({"1125515264",{"1120403456",48*4},{"1112014848",96*4}, {"1101004800",144*4}},gg.REGION_ANONYMOUS,gg.TYPE_QWORD)
+if #r == 0 then else
+searchEdit(48*4,32, 1065353216)
+searchEdit(96*4,32, 1065353216)
+searchEdit(144*4,32, 1065353216)
+searchEdit(23*4,32, 4294967325)
+searchEdit(24*4,32, 4294967297)
+searchEdit(25*4,32, 1)
+searchEdit(71*4,32, 4294967325)
+searchEdit(72*4,32, 4294967297)
+searchEdit(73*4,32, 1)
+searchEdit(119*4,32, 4294967325)
+searchEdit(120*4,32, 4294967297)
+searchEdit(121*4,32, 1)
+searchEdit(-23*4,32, 1)
+searchEdit(-24*4,32, 4294967297)
+searchEdit(-25*4,32, 4294967325)
+gg.clearResults()
+end
+gg.alert('✅𝙵𝚛𝚎𝚎 𝚂𝚑𝚘𝚙 𝙰𝚕𝚕 𝙰𝚌𝚝𝚒𝚟𝚊𝚝𝚎𝚍✅')
+end
+gg.clearResults()
+
+
+A ="- 𝚄𝚗𝚕𝚘𝚌𝚔 𝙲𝚑𝚎𝚊𝚝 𝙵𝚞𝚗𝚌𝚝𝚒𝚘𝚗🕶️\n- 𝙵𝚛𝚎𝚎 𝚂𝚑𝚘𝚙 𝙰𝚕𝚕🛒"
+end
+
+
+
+
+
+function Exit()
+gg.clearResults()
+gg.clearList()
+gg.toast('♥ᴍᴏᴅ ʙʏ ʜᴀ̉ɪ sᴄʀɪᴘᴛ♥')
+gg.alert("❤️Yᴏᴜᴛᴜʙᴇ :: ʜᴀ̉ɪ sᴄʀɪᴘᴛ🍀")
+print('☞♥Zᴀʟᴏ: 0375574755♥☜')
+os.exit()
+end
+while true do
+if gg.isVisible(true) then
+XGCK = 1
+gg.setVisible(false)
+end
+if XGCK == 1 then Main() end
+end
+
+
+
+
