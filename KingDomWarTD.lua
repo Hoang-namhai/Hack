9a4460342b133a54c95cd7da49857465a631d60f
Hoang-namhai <147267934+Hoang-namhai@users.noreply.github.com>
2024-12-01 18:42:52 +0700

    Update KingDomWarTD.lua

--- KingDomWarTD.lua
+++ KingDomWarTD.lua
@@ -41,7 +41,7 @@ function searchValue(t,hai1,hai2)
 rt={}
 gg.setRanges(hai1)
 gg.clearResults()
-
+gg.clearList()
 gg.setVisible(false)
 gg.searchNumber(t[1], hai2)
 local r = gg.getResults(99999999)
@@ -80,16 +80,63 @@ end
 gg.setValues(tt)
 end end
 
-r=searchValue({"4728779608739020800",{"1101004800",1*4},{"94D",-19*4}},32,32)
+r=searchValue({"4295411850",{"4295411850",0*4}},32,32)
 if #r == 0 then else
-searchEdit(-17*4,4, 1)
-searchEdit(-18*4,4, 1)
-searchEdit(-19*4,4, 29)
+searchEdit(0*1,1, 28)
+end
+
+
+function searchValue(t,hai1,hai2)
+rt={}
+gg.setRanges(hai1)
 gg.clearResults()
+gg.clearList()
+gg.setVisible(false)
+gg.searchNumber(t[1], hai2)
+local r = gg.getResults(99999999)
+if #r==0 then goto HoangNamHai end
+for it=2,#t do
+for i=1,#r do
+r[i].address=r[i].address+t[it][2]
+end
+local rr=gg.getValues(r)
+tt={}
+for i=1,#rr do
+   if rr[i].value== t[it][1] then
+   ii=#tt+1
+   tt[ii]={}
+   tt[ii].address=rr[i].address-t[it][2]
+   tt[ii].flags=4
+   end
+end
+if #tt==0 then goto HoangNamHai end
+r=gg.getValues(tt)
+if it==#t then rt=r goto HoangNamHai end
+end
+::HoangNamHai::
+return rt
+end
+
+function searchEdit(hnh1,hnh2,hnh3)
+if #r>0 then
+tt={}
+for i=1,#r do
+ii=#tt+1 tt[ii]={}
+tt[ii].address=r[i].address +hnh1
+tt[ii].flags=hnh2
+tt[ii].value=hnh3
+end
+gg.setValues(tt)
+end end
+
+r=searchValue({"12884901982",{"128849018883",1*4}},32,32)
+if #r == 0 then else
+searchEdit(0*4,1, "h 1D")
+searchEdit(1*4,1, "h 01")
+searchEdit(2*4,1, "h 01")
 end
 gg.alert('✅𝙵𝚁𝙴𝙴 𝚂𝙷𝙾𝙿 𝙰𝙻𝙻✅')
 gg.clearResults()
-end
 
 
 function EXITALL()
