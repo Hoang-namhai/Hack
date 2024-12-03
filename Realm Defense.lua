7ea7ee64be998392c3b824e91cf7e16e85ab681b
Hoang-namhai <147267934+Hoang-namhai@users.noreply.github.com>
2024-12-03 09:05:34 +0700

    Add files via upload

---
+++ Realm Defense.lua
@@ -0,0 +1,258 @@
+local info = gg.getTargetPackage() 
+if info == "com.babeltimeus.legendstd" then 
+else
+os.exit(print("♥𝑯𝒂̉𝒊 𝑺𝒄𝒓𝒊𝒑𝒕☠️\n \n• Vᴜɪ Lᴏ̀ɴɢ Cʜᴏ̣ɴ Đᴜ́ɴɢ Tʀᴏ̀ Cʜᴏ̛ɪ Cᴜ̉ᴀ Sᴄʀɪᴘᴛ •\n• ʜᴏᴀ̣̆ᴄ ᴄᴀ̣̂ᴘ ɴʜᴀ̣̂ᴛ ꜱᴄʀɪᴘᴛ ʙᴀ̉ɴ ᴜᴘᴅᴀᴛᴇ ᴍᴏ̛́ɪ ɴʜᴀ̂́ᴛ... •\n • Liên Hệ Zalo: 0375574755📱 •"))
+end 
+
+
+
+
+gg.sleep(100) 
+gg.getTargetInfo()
+gg.alert("𝙶𝙰𝙼𝙴 𝚅𝙴𝚁𝚂𝙸𝙾𝙽 : 3.3.5\n𝙶𝙰𝙼𝙴 𝙽𝙰𝙼𝙴 : 𝚁𝚎𝚊𝚕𝚖 𝙳𝚎𝚏𝚎𝚗𝚜𝚎\n𝚂𝙲𝚁𝙸𝙿𝚃 𝚅𝙴𝚁𝚂𝙸𝙾𝙽 : 1.0 Vip\n𝙻𝙰𝚂𝚃 𝚄𝙿𝙳𝙰𝚃𝙴 : 02/12/2024\nMᴀᴅᴇ Bʏ Hᴏᴀ̀ɴɢ Nᴀᴍ Hᴀ̉ɪ\nSᴄʀɪᴘᴛ Pᴜʙʟɪsʜᴇᴅ ɪɴ ©2024", "[𝙽𝙴𝚇𝚃]")
+gg.sleep(50)
+gg.sleep(50)
+
+
+
+
+
+
+
+
+
+
+function Main()
+HNHH = gg.multiChoice({
+"➣ D𝙰𝙼𝙰𝙶𝙴 𝙰𝚕𝚕🔥",
+"➣ S𝙿𝙰𝙼 𝙰𝚃𝚃𝙰𝙲𝙺💢",
+"➣ A𝚃𝚃𝙰𝙲𝙺 𝚂𝙿𝙴𝙴𝙳💦",
+"[𝙴𝚇𝙸𝚃]"
+}, nil,"❤Yᴏᴜᴛᴜʙᴇ :: ʜᴀ̉ɪ sᴄʀɪᴘᴛ🍀\n🎮G𝙰𝙼𝙴: 𝚁𝚎𝚊𝚕𝚖 𝙳𝚎𝚏𝚎𝚗𝚜𝚎\n💭V𝙴𝚁𝚂𝙸𝙾𝙽: 3.3.5  \n------------------------------------------------")
+if HNHH == nil then
+else
+if HNHH[1] == true then DamageAll() end
+if HNHH[2] == true then SpamAttack() end
+if HNHH[3] == true then AttackSpeed() end
+if HNHH[4] == true then EXITALL() end
+end
+XGCK = -1
+end
+
+
+function DamageAll()
+local menu = gg.alert('🅜🅔🅜🅤 ​ 🅓🅐🅜🅐🅖🅔 ​ 🅥🅘🅟\n❤Yᴏᴜᴛᴜʙᴇ :: ʜᴀ̉ɪ sᴄʀɪᴘᴛ🍀\n------------------------------------------------\n\n- F𝚄𝙽𝙲𝚃𝙸𝙾𝙽 𝙼𝙾𝙳:\n➣ D𝙰𝙼𝙰𝙶𝙴 𝙰𝚕𝚕🔥\n\n','[𝙾𝙽]','[𝙾𝙵𝙵]')
+if menu == nil then return 
+else
+if menu == 1 then OnDamageAll() end
+if menu == 2 then OffDamageAll() end
+end
+::ni::
+end
+
+
+
+function OnDamageAll()
+iTzMeCoder=gg.getRangesList('libil2cpp.so')[3].start
+iTzMe=nil  iTzMe={}
+iTzMe[1]={}
+iTzMe[1].address=iTzMeCoder+0x36329C+0
+iTzMe[1].value='128250C0h'
+iTzMe[1].flags=4
+iTzMe[2]={}
+iTzMe[2].address=iTzMeCoder+0x36329C+4
+iTzMe[2].value='72A9CA60h'
+iTzMe[2].flags=4
+iTzMe[3]={}
+iTzMe[3].address=iTzMeCoder+0x36329C+8
+iTzMe[3].value='1E270000h'
+iTzMe[3].flags=4
+iTzMe[4]={}
+iTzMe[4].address=iTzMeCoder+0x36329C+12
+iTzMe[4].value='D65F03C0h'
+iTzMe[4].flags=4
+gg.setValues(iTzMe)
+gg.toast('✅D𝙰𝙼𝙰𝙶𝙴 𝙰𝚕𝚕✅')
+DamageAll()
+end
+
+
+function OffDamageAll()
+iTzMeCoder=gg.getRangesList('libil2cpp.so')[3].start
+iTzMe=nil  iTzMe={}
+iTzMe[1]={}
+iTzMe[1].address=iTzMeCoder+0x36329C+0
+iTzMe[1].value='F81F0FFEh'
+iTzMe[1].flags=4
+iTzMe[2]={}
+iTzMe[2].address=iTzMeCoder+0x36329C+4
+iTzMe[2].value='F9406C08h'
+iTzMe[2].flags=4
+iTzMe[3]={}
+iTzMe[3].address=iTzMeCoder+0x36329C+8
+iTzMe[3].value='B4000188h'
+iTzMe[3].flags=4
+iTzMe[4]={}
+iTzMe[4].address=iTzMeCoder+0x36329C+12
+iTzMe[4].value='2D428102h'
+iTzMe[4].flags=4
+gg.setValues(iTzMe)
+gg.toast('❌D𝙰𝙼𝙰𝙶𝙴 𝙰𝚕𝚕❌')
+DamageAll()
+end
+
+
+
+
+
+
+function SpamAttack()
+local menu = gg.alert('🅢🅟🅐🅜 ​ 🅐🅣🅣🅐🅒🅚 ​ 🅥🅘🅟\n❤Yᴏᴜᴛᴜʙᴇ :: ʜᴀ̉ɪ sᴄʀɪᴘᴛ🍀\n------------------------------------------------\n\n- F𝚄𝙽𝙲𝚃𝙸𝙾𝙽 𝙼𝙾𝙳:\n➣ S𝙿𝙰𝙼 𝙰𝚃𝚃𝙰𝙲𝙺💦\n\n','[𝙾𝙽]','[𝙾𝙵𝙵]')
+if menu == nil then return 
+else
+if menu == 1 then OnSpamAttack() end
+if menu == 2 then OffSpamAttack() end
+end
+::ni::
+end
+
+
+
+function OnSpamAttack()
+iTzMeCoder=gg.getRangesList('libil2cpp.so')[3].start
+iTzMe=nil  iTzMe={}
+iTzMe[1]={}
+iTzMe[1].address=iTzMeCoder+0X3620E4+0
+iTzMe[1].value='128EA700h'
+iTzMe[1].flags=4
+iTzMe[2]={}
+iTzMe[2].address=iTzMeCoder+0X3620E4+4
+iTzMe[2].value='72A99520h'
+iTzMe[2].flags=4
+iTzMe[3]={}
+iTzMe[3].address=iTzMeCoder+0X3620E4+8
+iTzMe[3].value='1E270000h'
+iTzMe[3].flags=4
+iTzMe[4]={}
+iTzMe[4].address=iTzMeCoder+0X3620E4+12
+iTzMe[4].value='D65F03C0h'
+iTzMe[4].flags=4
+gg.setValues(iTzMe)
+gg.toast('✅S𝙿𝙰𝙼 𝙰𝚃𝚃𝙰𝙲𝙺✅')
+SpamAttack()
+end
+
+
+
+function OffSpamAttack()
+iTzMeCoder=gg.getRangesList('libil2cpp.so')[3].start
+iTzMe=nil  iTzMe={}
+iTzMe[1]={}
+iTzMe[1].address=iTzMeCoder+0X3620E4+0
+iTzMe[1].value='F81F0FFEh'
+iTzMe[1].flags=4
+iTzMe[2]={}
+iTzMe[2].address=iTzMeCoder+0X3620E4+4
+iTzMe[2].value='F9405808h'
+iTzMe[2].flags=4
+iTzMe[3]={}
+iTzMe[3].address=iTzMeCoder+0X3620E4+8
+iTzMe[3].value='B4000188h'
+iTzMe[3].flags=4
+iTzMe[4]={}
+iTzMe[4].address=iTzMeCoder+0X3620E4+12
+iTzMe[4].value='2D428102h'
+iTzMe[4].flags=4
+gg.setValues(iTzMe)
+gg.toast('❌S𝙿𝙰𝙼 𝙰𝚃𝚃𝙰𝙲𝙺❌')
+SpamAttack()
+end
+
+
+
+
+
+
+
+
+function AttackSpeed()
+local menu = gg.alert('🅐🅣🅣🅐🅒🅚 ​ 🅢🅟🅔🅔🅓 ​ 🅥🅘🅟\n❤Yᴏᴜᴛᴜʙᴇ :: ʜᴀ̉ɪ sᴄʀɪᴘᴛ🍀\n------------------------------------------------\n\n- F𝚄𝙽𝙲𝚃𝙸𝙾𝙽 𝙼𝙾𝙳:\n➣ A𝚃𝚃𝙰𝙲𝙺 𝚂𝙿𝙴𝙴𝙳💕\n\n','[𝙾𝙽]','[𝙾𝙵𝙵]')
+if menu == nil then return 
+else
+if menu == 1 then OnAttackSpeed() end
+if menu == 2 then OffAttackSpeed() end
+end
+::ni::
+end
+
+
+
+function OnAttackSpeed()
+iTzMeCoder=gg.getRangesList('libil2cpp.so')[3].start
+iTzMe=nil  iTzMe={}
+iTzMe[1]={}
+iTzMe[1].address=iTzMeCoder+0X36451C+0
+iTzMe[1].value='52800000h'
+iTzMe[1].flags=4
+iTzMe[2]={}
+iTzMe[2].address=iTzMeCoder+0X36451C+4
+iTzMe[2].value='72A83400h'
+iTzMe[2].flags=4
+iTzMe[3]={}
+iTzMe[3].address=iTzMeCoder+0X36451C+8
+iTzMe[3].value='1E270000h'
+iTzMe[3].flags=4
+iTzMe[4]={}
+iTzMe[4].address=iTzMeCoder+0X36451C+12
+iTzMe[4].value='D65F03C0h'
+iTzMe[4].flags=4
+gg.setValues(iTzMe)
+gg.toast('✅A𝚃𝚃𝙰𝙲𝙺 𝚂𝙿𝙴𝙴𝙳✅')
+AttackSpeed()
+end
+
+
+function OffAttackSpeed()
+iTzMeCoder=gg.getRangesList('libil2cpp.so')[3].start
+iTzMe=nil  iTzMe={}
+iTzMe[1]={}
+iTzMe[1].address=iTzMeCoder+0X36451C+0
+iTzMe[1].value='F81F0FFEh'
+iTzMe[1].flags=4
+iTzMe[2]={}
+iTzMe[2].address=iTzMeCoder+0X36451C+4
+iTzMe[2].value='F9405C08h'
+iTzMe[2].flags=4
+iTzMe[3]={}
+iTzMe[3].address=iTzMeCoder+0X36451C+8
+iTzMe[3].value='B4000188h'
+iTzMe[3].flags=4
+iTzMe[4]={}
+iTzMe[4].address=iTzMeCoder+0X36451C+12
+iTzMe[4].value='2D428102h'
+iTzMe[4].flags=4
+gg.setValues(iTzMe)
+gg.toast('❌A𝚃𝚃𝙰𝙲𝙺 𝚂𝙿𝙴𝙴𝙳❌')
+AttackSpeed()
+end
+
+
+
+
+
+function EXITALL()
+gg.toast('♥ᴍᴏᴅ ʙʏ ʜᴀ̉ɪ sᴄʀɪᴘᴛ♥')
+gg.alert("❤️Yᴏᴜᴛᴜʙᴇ :: ʜᴀ̉ɪ sᴄʀɪᴘᴛ🍀")
+print('☞♥Zᴀʟᴏ: 0375574755♥☜')
+os.exit()
+end
+while true do
+if gg.isVisible(true) then
+XGCK = 1
+gg.setVisible(false)
+end
+gg.clearResults()
+if XGCK == 1 then
+Main()
+end
+end
