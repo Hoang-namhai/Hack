58f017c5b1c5c48825d10357051db2a4eddf1601
Hoang-namhai <147267934+Hoang-namhai@users.noreply.github.com>
2024-12-30 11:38:48 +0700

    Add files via upload

---
+++ TiaDaiV5.lua
@@ -0,0 +1,127 @@
+gg.alert("♥Hᴀᴄᴋ Mᴏᴅ Bʏ Hᴀ̉ɪ Sᴄʀɪᴘᴛ♥")
+function Main()
+HNH = gg.alert('❤𝒀𝒐𝒖𝒕𝒖𝒃𝒆 :: 𝑯𝒂̉𝒊 𝑺𝒄𝒓𝒊𝒑𝒕🍀 𝒗𝒆𝒓𝒔𝒊𝒐𝒏 𝟏.𝟎.𝟏\
+🔥𝒁𝒂𝒍𝒐: 𝟎𝟑𝟓𝟖𝟏𝟖𝟒𝟒𝟓𝟒📲.','๑𝑻𝒉𝒐𝒂́𝒕๑','๑𝑩𝒂̣̂𝒕 𝑯𝒂𝒄𝒌๑')
+if HNH==1 then Exit() end
+if HNH==2 then a1() end
+XGCK=-1
+end
+function a1()
+function searchValue(t,hai1,hai2)
+rt={}
+gg.setRanges(hai1)
+gg.clearResults()
+gg.clearList()
+gg.setVisible(false)
+gg.searchNumber(t[1], hai2)
+local r = gg.getResults(99999999)
+if #r==0 then goto HoangNamHai end
+for it=2,#t do
+for i=1,#r do
+r[i].address=r[i].address+t[it][2]
+end
+local rr=gg.getValues(r)
+tt={}
+for i=1,#rr do
+   if rr[i].value== t[it][1] then
+   ii=#tt+1
+   tt[ii]={}
+   tt[ii].address=rr[i].address-t[it][2]
+   tt[ii].flags=4
+   end
+end
+if #tt==0 then goto HoangNamHai end
+r=gg.getValues(tt)
+if it==#t then rt=r goto HoangNamHai end
+end
+::HoangNamHai::
+return rt
+end
+
+function searchEdit(hnh1,hnh2,hnh3)
+if #r>0 then
+tt={}
+for i=1,#r do
+ii=#tt+1 tt[ii]={}
+tt[ii].address=r[i].address +hnh1
+tt[ii].flags=hnh2
+tt[ii].value=hnh3
+end
+gg.setValues(tt)
+end end
+
+r=searchValue({"1075196723",{" 1070176665",-6*4},{"1075196723",2*4}, {" 1072273817",-2*4}},gg.REGION_C_ALLOC,gg.TYPE_DWORD)
+if #r == 0 then else
+searchEdit( -8*4,4,-1054972314)
+gg.clearResults()
+end
+function searchValue(t,hai1,hai2)
+rt={}
+gg.setRanges(hai1)
+gg.clearResults()
+gg.clearList()
+gg.setVisible(false)
+gg.searchNumber(t[1], hai2)
+local r = gg.getResults(99999999)
+if #r==0 then goto HoangNamHai end
+for it=2,#t do
+for i=1,#r do
+r[i].address=r[i].address+t[it][2]
+end
+local rr=gg.getValues(r)
+tt={}
+for i=1,#rr do
+   if rr[i].value== t[it][1] then
+   ii=#tt+1
+   tt[ii]={}
+   tt[ii].address=rr[i].address-t[it][2]
+   tt[ii].flags=4
+   end
+end
+if #tt==0 then goto HoangNamHai end
+r=gg.getValues(tt)
+if it==#t then rt=r goto HoangNamHai end
+end
+::HoangNamHai::
+return rt
+end
+
+function searchEdit(hnh1,hnh2,hnh3)
+if #r>0 then
+tt={}
+for i=1,#r do
+ii=#tt+1 tt[ii]={}
+tt[ii].address=r[i].address +hnh1
+tt[ii].flags=hnh2
+tt[ii].value=hnh3
+end
+gg.setValues(tt)
+end end
+
+gg.toast("๑Hᴀᴄᴋ Tʜᴀ̀ɴʜ Cᴏ̂ɴɢ๑")
+r=searchValue({" 1070931640",{" -1717986918",-5*4},{" 1374389535",-1*4}, {" 1071644672",-2*4}},gg.REGION_C_ALLOC,gg.TYPE_DWORD)
+if #r == 0 then else
+searchEdit(-4*4,4, -1072064102)
+gg.toast("๑Hᴀᴄᴋ Tʜᴀ̀ɴʜ Cᴏ̂ɴɢ๑")
+gg.clearResults()
+end
+end
+
+function Exit()
+print("- 🇻🇳𝑭𝒐𝒍𝒍𝒐𝒘 𝒀𝒐𝒖𝒕𝒖𝒃𝒆 :: 𝑯𝒂̉𝒊 𝑺𝒄𝒓𝒊𝒑𝒕🇻🇳")
+gg.alert("♥𝑪𝒉𝒖́𝒄 𝑩𝒂̣𝒏 𝟏 𝑵𝒈𝒂̀𝒚 𝑽𝒖𝒊 𝑽𝒆̉!!!♥")
+print("- 𝒁𝒂𝒍𝒐: 𝟎𝟑𝟓.𝟖𝟏𝟖.𝟒𝟒𝟓𝟒 📲.")
+print("- ❤𝐘𝐨𝐮𝐭𝐮𝐛𝐞 :: 𝐇𝐚̉𝐢 𝐒𝐜𝐫𝐢𝐩𝐭🍀.")
+print("- 𝑯𝒂̉𝒊 𝑺𝒄𝒓𝒊𝒑𝒕 𝑪𝒉𝒖𝒚𝒆̂𝒏 𝑻𝒐𝒐𝒍𝒔 𝑶𝒓 𝑯𝒂𝒄𝒌 𝑩𝒊𝒅𝒂 𝒁𝒊𝒏𝒈𝒑𝒍𝒂𝒚 !")
+os.exit()
+end
+while true do
+if gg.isVisible(true) then
+XGCK = 1
+gg.setVisible(false)
+end
+gg.clearResults()
+if XGCK == 1 then
+Main()
+end
+end
