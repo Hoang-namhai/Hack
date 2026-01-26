a44f251ba02ba2c949a42343c7d1623dc604593f
Hoàng Nam Hải <147267934+Hoang-namhai@users.noreply.github.com>
2026-01-26 23:15:48 +0700

    Update Tɪᴀ Tᴏ.lua

--- Tɪᴀ Tᴏ.lua
+++ Tɪᴀ Tᴏ.lua
@@ -1,2 +1,140 @@
-gg.alert("𝑯𝒊𝒆̣̂𝒏 𝑻𝒂̣𝒊 𝑨𝑫𝑴𝑰𝑵 𝑫𝒂̃ 𝑹𝒂 𝑩𝒂̉𝒏 𝑴𝒐̛́𝒊 𝑻𝒓𝒐𝒏𝒈 𝑵𝒉𝒐́𝒎 𝒁𝒂𝒍𝒐 𝑨𝑬 𝑽𝒐̂ 𝒁𝒂𝒍𝒐 𝑫𝒆̂̉ 𝑳𝒂̂́𝒚 𝑵𝒉𝒆́.
-𝑰𝒃𝒐𝒙 𝒁𝒂𝒍𝒐 𝒂𝒅𝒎𝒊𝒏 𝟎𝟑𝟓.𝟖𝟏𝟖.𝟒𝟒𝟓𝟒!")
+
+function Main()
+menu = gg.multiChoice({
+A .."", 
+"๑𝑻𝒉𝒐𝒂́𝒕๑"},nil,"                   ⚡❤𝐘𝐨𝐮𝐭𝐮𝐛𝐞 :: 𝐇𝐚̉𝐢 𝐒𝐜𝐫𝐢𝐩𝐭🍀⚡\
+   ๑۩۞۩๑•❅──────✧❅✦❅✧──────❅•๑۩۞۩๑\
+                          ⚡𝐌𝐨𝐝 𝐁𝐲 𝐇𝐚̉𝐢 𝐒𝐜𝐫𝐢𝐩𝐭⚡\
+   ๑۩۞۩๑•❅──────✧❅✦❅✧──────❅•๑۩۞۩๑\
+ㅤㅤ\
+ㅤ ㅤ")
+if menu == nil then else
+if menu[1] == true then a1() end
+if menu[2] == true then Exit() end
+end
+XGCK = -1
+end
+A ="〖๑𝑩𝒂̣̂𝒕 𝑻𝒊𝒂 𝑻𝒐๑〗 ✅"
+function a1() 
+if A =="〖๑𝑩𝒂̣̂𝒕 𝑻𝒊𝒂 𝑻𝒐๑〗 ✅"  then
+----Dán Code Vào Đây----
+function searchValue(t,hai1,hai2)
+rt={}
+gg.setRanges(hai1)
+gg.clearResults()
+gg.clearList()
+gg.setVisible(false)
+gg.searchNumber(t[1], hai2)
+local r = gg.getResults(99999999)
+if #r==0 then goto HoangNamHai end
+for it=2,#t do
+for i=1,#r do
+r[i].address=r[i].address+t[it][2]
+end
+local rr=gg.getValues(r)
+tt={}
+for i=1,#rr do
+   if rr[i].value== t[it][1] then
+   ii=#tt+1
+   tt[ii]={}
+   tt[ii].address=rr[i].address-t[it][2]
+   tt[ii].flags=4
+   end
+end
+if #tt==0 then goto HoangNamHai end
+r=gg.getValues(tt)
+if it==#t then rt=r goto HoangNamHai end
+end
+::HoangNamHai::
+return rt
+end
+
+function searchEdit(hnh1,hnh2,hnh3)
+if #r>0 then
+tt={}
+for i=1,#r do
+ii=#tt+1 tt[ii]={}
+tt[ii].address=r[i].address +hnh1
+tt[ii].flags=hnh2
+tt[ii].value=hnh3
+end
+gg.setValues(tt)
+end end
+
+gg.toast("๑𝑩𝒂̣̂𝒕 𝑯𝒂𝒄𝒌 𝑶𝑲๑")
+r=searchValue({" 1070931640",{" -1717986918",-5*4},{" 1374389535",-1*4}, {" 1071644672",-2*4}},gg.REGION_C_ALLOC,gg.TYPE_DWORD)
+if #r == 0 then else
+searchEdit(-4*4,4, - 1054972314)
+gg.toast("๑𝑩𝒂̣̂𝒕 𝑯𝒂𝒄𝒌 𝑶𝑲๑")
+gg.clearResults()
+end
+A = "〖๑𝑻𝒂̆́𝒕 𝑻𝒊𝒂 𝑻𝒐๑〗❌" 
+elseif A == "〖๑𝑻𝒂̆́𝒕 𝑻𝒊𝒂 𝑻𝒐๑〗❌" then 
+----Dán Code Vào Đây----
+function searchValue(t,hai1,hai2)
+rt={}
+gg.setRanges(hai1)
+gg.clearResults()
+gg.clearList()
+gg.setVisible(false)
+gg.searchNumber(t[1], hai2)
+local r = gg.getResults(99999999)
+if #r==0 then goto HoangNamHai end
+for it=2,#t do
+for i=1,#r do
+r[i].address=r[i].address+t[it][2]
+end
+local rr=gg.getValues(r)
+tt={}
+for i=1,#rr do
+   if rr[i].value== t[it][1] then
+   ii=#tt+1
+   tt[ii]={}
+   tt[ii].address=rr[i].address-t[it][2]
+   tt[ii].flags=4
+   end
+end
+if #tt==0 then goto HoangNamHai end
+r=gg.getValues(tt)
+if it==#t then rt=r goto HoangNamHai end
+end
+::HoangNamHai::
+return rt
+end
+
+function searchEdit(hnh1,hnh2,hnh3)
+if #r>0 then
+tt={}
+for i=1,#r do
+ii=#tt+1 tt[ii]={}
+tt[ii].address=r[i].address +hnh1
+tt[ii].flags=hnh2
+tt[ii].value=hnh3
+end
+gg.setValues(tt)
+end end
+
+gg.toast("๑𝑻𝒂̆́𝒕 𝑯𝒂𝒄𝒌 𝑶𝑲๑")
+r=searchValue({" 1070931640",{" -1717986918",-5*4},{" 1374389535",-1*4}, {" 1071644672",-2*4}},gg.REGION_C_ALLOC,gg.TYPE_DWORD)
+if #r == 0 then else
+searchEdit(-4*4,4,1069128089)
+gg.toast("๑𝑻𝒂̆́𝒕 𝑯𝒂𝒄𝒌 𝑶𝑲๑")
+gg.clearResults()
+end
+A ="〖๑𝑩𝒂̣̂𝒕 𝑻𝒊𝒂 𝑻𝒐๑〗 ✅"
+end
+end
+function Exit()
+print("- ❤𝐘𝐨𝐮𝐭𝐮𝐛𝐞 :: 𝐇𝐚̉𝐢 𝐒𝐜𝐫𝐢𝐩𝐭🍀.")
+print("- 𝑯𝒂̉𝒊 𝑺𝒄𝒓𝒊𝒑𝒕 𝑪𝒉𝒖𝒚𝒆̂𝒏 𝑻𝒐𝒐𝒍𝒔 𝑶𝒓 𝑯𝒂𝒄𝒌 𝑩𝒊𝒅𝒂 𝒁𝒊𝒏𝒈𝒑𝒍𝒂𝒚 !")
+os.exit()
+end
+while true do
+if gg.isVisible(true) then
+XGCK = 1
+gg.setVisible(false)
+end
+if XGCK == 1 then Main() end
+end
+
+
